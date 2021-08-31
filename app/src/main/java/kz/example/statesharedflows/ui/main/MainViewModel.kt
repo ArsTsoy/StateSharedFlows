2ebc2393cd3a2f3a3930fd5f41a3d0891430ba39
Arslan Tsoy <tsoy.ar@rahmetapp.kz>
2021-08-31 17:23:12 +0600

    add

--- app/src/main/java/kz/example/statesharedflows/ui/main/MainViewModel.kt
+++ app/src/main/java/kz/example/statesharedflows/ui/main/MainViewModel.kt
@@ -1,6 +1,5 @@
 package kz.example.statesharedflows.ui.main
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
